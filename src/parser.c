ad3241a65c4ce050fc83d324efd654a3eb1bcccd
Ian Moffett <ian@lunota.org>
2026-01-10 16:27:43 -0500

    parser: Seperate TT_IDENT parsing to seperate func
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- src/parser.c
+++ src/parser.c
@@ -455,10 +455,50 @@ parse_endscope(struct gup_state *state, tt_t scope_tok)
 }
 
 static int
-begin_parse(struct gup_state *state, struct token *tok)
+parse_ident(struct gup_state *state, struct token *tok)
 {
     struct ast_node *root;
     struct symbol *symbol;
+
+    if (state == NULL || tok == NULL) {
+        return -EINVAL;
+    }
+
+    symbol = symbol_from_name(&state->g_symtab, tok->s);
+    if (symbol == NULL) {
+        trace_error(
+            state,
+            "implicit declaration of function \"%s\"\n",
+            tok->s
+        );
+
+        return -1;
+    }
+
+    if (parse_expect(state, tok, TT_LPAREN) < 0) {
+        return -1;
+    }
+
+    /* Expect () if we see '(' */
+    if (tok->type == TT_LPAREN) {
+        if (parse_expect(state, tok, TT_RPAREN) < 0)
+            return -1;
+    }
+
+    if (ast_node_alloc(state, AST_OP_CALL, &root) < 0) {
+        trace_warn("[PARSER] retvoid failure\n");
+        return -1;
+    }
+
+    root->symbol = symbol;
+    cg_compile_node(state, root);
+    return 0;
+}
+
+static int
+begin_parse(struct gup_state *state, struct token *tok)
+{
+    struct ast_node *root;
     tt_t scope_tok;
 
     if (state == NULL || tok == NULL) {
@@ -572,34 +612,9 @@ begin_parse(struct gup_state *state, struct token *tok)
         cg_compile_node(state, root);
         break;
     case TT_IDENT:
-        symbol = symbol_from_name(&state->g_symtab, tok->s);
-        if (symbol == NULL) {
-            trace_error(
-                state,
-                "implicit declaration of function \"%s\"\n",
-                tok->s
-            );
-
+        if (parse_ident(state, tok) < 0) {
             return -1;
         }
-
-        if (parse_expect(state, tok, TT_LPAREN) < 0) {
-            return -1;
-        }
-
-        /* Expect () if we see '(' */
-        if (tok->type == TT_LPAREN) {
-            if (parse_expect(state, tok, TT_RPAREN) < 0)
-                return -1;
-        }
-
-        if (ast_node_alloc(state, AST_OP_CALL, &root) < 0) {
-            trace_warn("[PARSER] retvoid failure\n");
-            return -1;
-        }
-
-        root->symbol = symbol;
-        cg_compile_node(state, root);
         break;
     default:
         break;
