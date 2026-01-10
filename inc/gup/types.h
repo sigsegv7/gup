fe65d79f2d7a86611d329328e72e374060109e06
Ian Moffett <ian@lunota.org>
2026-01-09 22:44:32 -0500

    gup: types: Move gup data types to enum
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- inc/gup/types.h
+++ inc/gup/types.h
@@ -6,15 +6,14 @@
 #ifndef GUP_TYPES_H
 #define GUP_TYPES_H 1
 
-#include <stdint.h>
-
-typedef uint8_t gup_type_t;
-
-#define GUP_TYPE_BAD    0x00
-#define GUP_TYPE_VOID   0x01
-#define GUP_TYPE_U8     0x02
-#define GUP_TYPE_U16    0x03
-#define GUP_TYPE_U32    0x04
-#define GUP_TYPE_U64    0x05
+typedef enum {
+    GUP_TYPE_BAD,
+    GUP_TYPE_VOID,
+    GUP_TYPE_U8,
+    GUP_TYPE_U16,
+    GUP_TYPE_U32,
+    GUP_TYPE_U64,
+    GUP_TYPE_MAX
+} gup_type_t;
 
 #endif  /* !GUP_TYPES_H */
